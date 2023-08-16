b3afa0873e5f75707279025c5441b4b383f3655d
Haritanagarajan <30harita2002@gmail.com>
2023-08-16 15:15:59 +0530

    Payment

--- src/App.jsx
+++ src/App.jsx
@@ -12,27 +12,31 @@ import Moviedisplay from './Components/Homepage/Homedisplay';
 import { ToastContainer } from 'react-toastify';
 import ChooseTickets from './Components/SeatRoom/ChooseTickets';
 import Movie from './Components/Homepage/Movie';
-import { Tamildisplay ,Malayalamdisplay ,Telugudisplay} from './Components/Homepage/Movie';
+import Next from './Components/Payment/Next';
+import PaymentSummary from './Components/Payment/PaymentSummary';
+import { Tamildisplay, Malayalamdisplay, Telugudisplay } from './Components/Homepage/Movie';
 function App() {
-
   return (
     <div className="App">
       <BrowserRouter>
         <CustomNavbar />
         <Routes>
           <Route path="/" element={<MovieList />} />
-          <Route path="/movies" element={<Movie/>} />
-          <Route path="/Tamilmovies/:id" element={<Tamildisplay/>} />
-          <Route path="/MalayalamMovies/:id" element={<Malayalamdisplay/>} />
-          <Route path="/TeluguMovies/:id" element={<Telugudisplay/>} />
+          <Route path="/movies" element={<Movie />} />
+          <Route path="/Tamilmovies/:id" element={<Tamildisplay />} />
+          <Route path="/MalayalamMovies/:id" element={<Malayalamdisplay />} />
+          <Route path="/TeluguMovies/:id" element={<Telugudisplay />} />
           <Route path="/movie/:id" element={<Moviedisplay />} />
           <Route path="/register" element={<Register />} />
           <Route path="/ChooseTickets" element={< ChooseTickets />} />
           <Route path="/Theater" element={<Theater />} />
           <Route path="/signin" element={<SignIn />} />
+          {/* // */}
+          <Route path="/Next" element={<Next />} />
+          <Route path="/PaymentSummary" element={<PaymentSummary />} />
+
         </Routes>
         <ToastContainer position="top-right" autoClose={3000} />
-
       </BrowserRouter>
     </div>
   );
