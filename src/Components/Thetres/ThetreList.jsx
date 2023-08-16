5c88b4262faa7caa96c278eb175cfaa1c353cbdb
ikarthi2108 <karthicloud6@gmailcom>
2023-08-16 13:04:46 +0530

    kk 16aug 1 st commit

---
+++ src/Components/Thetres/ThetreList.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../styles/ThetreList.css';
+
+const ThetreList = () => {
+  const [selectedLocation, setSelectedLocation] = useState('chennai');
+  const [selectedMovie, setSelectedMovie] = useState('');
+  const [theaterData, setTheaterData] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:4000/ThetreList')
+      .then(response => {
+        setTheaterData(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching theater data:', error);
+      });
+  }, []);
+
+  const filteredTheaters = theaterData.filter(theater => {
+    return (
+      theater.location.toLowerCase() === selectedLocation &&
+      (selectedMovie === '' || theater.MoviesRunning1 === selectedMovie)
+    );
+  });
+
+  return (
+    <div id="thetre-list" className="theatre-list-container">
+      <h1>Theatre List</h1>
+      <div className="label-container">
+        <label className="label" htmlFor="locationSelect">Select Location:</label>
+        <select className="select-box" id="locationSelect" value={selectedLocation} onChange={e => setSelectedLocation(e.target.value)}>
+          <option value="chennai">Chennai</option>
+          <option value="mumbai">Mumbai</option>
+        </select>
+      </div>
+      <div className="theatre-cards">
+        {filteredTheaters.map((theater, index) => (
+          <div key={index} className="card">
+            <h3>{theater.Name}</h3>
+            <p><strong>Location:</strong> {theater.location}</p>
+            <p><strong>Movies Running:</strong> {theater.MoviesRunning1}</p>
+            <button className="book-now-button">Book Now</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ThetreList;
