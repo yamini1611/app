1a9eb1862d66d4916bc63306b51a6eed2b394394
Darshan <priyadarshanmanoharan@gmail.com>
2023-08-16 14:33:11 -0700

    Error with the count- Darshan

--- src/Components/SeatRoom/Theater.jsx
+++ src/Components/SeatRoom/Theater.jsx
@@ -32,12 +32,12 @@ fetchingTickets();
         <h6 className="text-secondary mt-2"> E l i t e </h6>
       </Divider>
 
-      <SeatLayout noOfSeats={props.noOfSeats} rows={eliteRows} category="E" />
+      <SeatLayout noOfSeats={props.noOfSeats}  rows={eliteRows} category="E" />
 
       <Divider className="text-center  col-lg-5 mx-auto p-1">
         <h6 className="text-secondary mt-2"> B u d g e t </h6>
       </Divider>
-      <SeatLayout rows={budgetRows} divider={true} category="B" />
+      <SeatLayout noOfSeats={props.noOfSeats} rows={budgetRows} divider={true} category="B" />
 
       <Card className="mx-auto sticky-bottom">
         <div className="row container mx-auto">
