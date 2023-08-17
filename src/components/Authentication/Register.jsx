76853c940f292c42c74473046e8a525107a2178d
ikarthi2108 <karthicloud6@gmailcom>
2023-08-17 10:09:42 +0530

    kk register commit

--- src/components/Authentication/Register.jsx
+++ src/components/Authentication/Register.jsx
@@ -1,8 +1,3 @@
-
-
-
-
-
 import { Link } from "react-router-dom";
 import axios from "axios";
 import "../styles/Register.css";
@@ -10,20 +5,27 @@ import { useEffect, useState } from "react";
 import React from "react";
 import GSI from "../Google/GSI";
 import { Divider } from "@mui/material";
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Register = () => {
-  const [fullName, setFullName] = useState();
+  const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [emailValid, setEmailValid] = useState(false);
   const [passwordMatch, setPasswordMatch] = useState(false);
+  const [phoneNumberValid, setPhoneNumberValid] = useState(false);
 
   const validateEmail = (email) => {
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    return emailRegex.test(email)
-;
+    return emailRegex.test(email);
+  };
+
+  const validatePhoneNumber = (phoneNumber) => {
+    return phoneNumber.length >= 10; // Basic length check
   };
 
   const handleEmailChange = (e) => {
@@ -41,14 +43,20 @@ const Register = () => {
     setPasswordMatch(e.target.value === password);
   };
 
+  const handlePhoneNumberChange = (e) => {
+    setPhoneNumber(e.target.value);
+    setPhoneNumberValid(validatePhoneNumber(e.target.value));
+  };
+
   const handleRegister = () => {
     const isFullNameValid = /^[A-Za-z ]{1,30}$/.test(fullName);
     const isPasswordValid =
       /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/.test(
         password
       );
+    const isPhoneNumberValid = validatePhoneNumber(phoneNumber);
 
-    if (!fullName || !email || !password || !confirmPassword) {
+    if (!fullName || !email || !password || !confirmPassword || !phoneNumber) {
       setErrorMessage("All fields are required.");
     } else if (!isFullNameValid) {
       setErrorMessage(
@@ -62,11 +70,14 @@ const Register = () => {
       );
     } else if (!passwordMatch) {
       setErrorMessage("Passwords do not match.");
+    } else if (!isPhoneNumberValid) {
+      setErrorMessage("Please enter a valid phone number.");
     } else {
       const userData = {
         fullName,
         email,
         password,
+        phoneNumber,
         isLogged: false,
       };
 
@@ -81,6 +92,10 @@ const Register = () => {
               .post("http://localhost:4000/Register", userData)
               .then((response) => {
                 console.log("Registration successful!", response.data);
+                // Show toast message
+                toast.success('Registration successful!', {
+                  position: toast.POSITION.TOP_CENTER
+                });
               })
               .catch((error) => {
                 console.error("Registration failed:", error);
@@ -90,7 +105,6 @@ const Register = () => {
         });
     }
   };
-
   return (
     <div className="register-page">
       <div className="register-container">
@@ -143,8 +157,20 @@ const Register = () => {
             className={`validation-symbol ${confirmPassword ? "green" : "red"}`}
           ></span>
         </div>
+        <div className="input-container">
+          <input
+            className={`input-field ${phoneNumberValid ? "valid" : "invalid"}`}
+            type="tel"
+            placeholder="Phone Number"
+            value={phoneNumber}
+            onChange={handlePhoneNumberChange}
+          />
+          <span className={`validation-symbol ${phoneNumber ? "green" : "red"}`}>
+            {phoneNumber ? "\u2713" : "\u2718"}
+          </span>
+        </div>
         {errorMessage && <p className="error-message">{errorMessage}</p>}
-        <Divider className="text-center  col-lg-5 mx-auto  p-3">or</Divider>
+        <Divider className="text-center col-lg-5 mx-auto p-3">or</Divider>
         <div className="pb-3 justify-content-center d-flex">
           <GSI type="signin" axios="post"></GSI>
         </div>
@@ -157,9 +183,11 @@ const Register = () => {
             <Link to="/signin">Login</Link>
           </span>
         </p>
+        <ToastContainer />
       </div>
+  
     </div>
   );
-};
+}
 
 export default Register;
