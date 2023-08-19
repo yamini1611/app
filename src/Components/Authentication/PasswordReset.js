e3b8d02d893bb2a8e87137848c6eb318f61a1486
ikarthi2108 <karthicloud6@gmailcom>
2023-08-19 16:24:42 +0530

    otp kk commit

--- src/Components/Authentication/PasswordReset.js
+++ src/Components/Authentication/PasswordReset.js
@@ -1,51 +1,57 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import axios from 'axios';
-import '../styles/PasswordReset.css'
+import emailjs from '@emailjs/browser';
 
 const PasswordReset = () => {
-  const [email, setEmail] = useState('');
+  const form = useRef();
   const [message, setMessage] = useState('');
+  
+  const sendEmail = async (e) => {
+    e.preventDefault();
 
-  const handlePasswordReset = () => {
-    if (!email) {
-      setMessage('Please enter your email.');
-    } else {
-      axios
-        .get(`http://localhost:4000/Register/?email=${email}`)
-        .then((response) => {
-          if (response.data.length > 0) {
-           
-            setMessage(`Password reset link sent to ${email}.`);
-          } else {
-            setMessage('No account found with this email.');
-          }
-        })
-        .catch((error) => {
-          console.error('Password reset failed:', error);
-          setMessage('Password reset failed. Please try again.');
-        });
+    const formData = new FormData(form.current);
+    const enteredEmail = formData.get('message');
+
+    try {
+      const response = await axios.get('http://localhost:4000/Register');
+      const registeredUsers = response.data;
+      
+      const user = registeredUsers.find(user => user.email === enteredEmail);
+      if (user) {
+        // Generate a random 4-digit code
+        const randomCode = Math.floor(1000 + Math.random() * 9000);
+        
+        // Configure and send the email
+        const emailData = {
+          from_name: 'Your App',
+          from_email: 'yourapp@example.com',
+          message: `Your verification code: ${randomCode}`,
+          to_name: user.fullName,
+          to_email: enteredEmail,
+        };
+
+        await emailjs.send('service_tz8tvk8', 'template_vantckm', emailData, 'wKE-SxLbsRZble8LF');
+        
+        setMessage(`Verification code sent to ${enteredEmail}`);
+      } else {
+        setMessage('Email not registered');
+      }
+    } catch (error) {
+      console.error(error);
+      setMessage('An error occurred');
     }
   };
+
   return (
-    <div className="password-reset-container">
-      <h2 className="password-reset-header">Forgot Password</h2>
-      <p>Enter your email to receive a password reset link.</p>
-      <div className="password-reset-form">
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <button onClick={handlePasswordReset}>Reset Password</button>
-        <p className="password-reset-message">{message}</p>
-      </div>
+    <div>
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Enter your registered email:</label>
+        <input type="email" name="message" />
+        <input type="submit" value="Send" />
+      </form>
+      <p>{message}</p>
     </div>
   );
 };
 
 export default PasswordReset;
-
-
