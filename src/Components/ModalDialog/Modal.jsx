bbdc9de1925950be220ff9927cb2dda198ed3dd0
ikarthi2108 <karthicloud6@gmailcom>
2023-08-25 18:26:35 +0530

    last commit

--- src/Components/ModalDialog/Modal.jsx
+++ src/Components/ModalDialog/Modal.jsx
@@ -1,25 +1,28 @@
 import React, { useState } from "react";
 import { Button } from "primereact/button";
 import { Card } from "primereact/card";
+import { Dialog } from "@mui/material";
+import Theater from "../SeatRoom/Theater";
 
 import cycle from "../Assets/Images/cycle.jpg";
 import bike3 from "../Assets/Images/bike3.jpg";
 import car3 from "../Assets/Images/car3.jpg";
 import car4 from "../Assets/Images/car4.jpg";
 import car5 from "../Assets/Images/car5.jpg";
-
 import bigcar from "../Assets/Images/bigcar.jpg";
 import bus from "../Assets/Images/bus.jpg";
 
-import "../styles/ResponsiveDialog.css"; 
+import "../styles/ResponsiveDialog.css";
 
 export default function ResponsiveDialog(props) {
   const [selectedSeat, setSelectedSeat] = useState(null);
+  const [visible, setVisible] = useState(false);
+  const [proceedClicked, setProceedClicked] = useState(false);
 
   const seatImageMap = {
     1: cycle,
     2: bike3,
-    3:car3,
+    3: car3,
     4: car4,
     5: car5,
     6: car5,
@@ -29,8 +32,13 @@ export default function ResponsiveDialog(props) {
     10: bus,
   };
 
-  const handleSelect = (seatNumber) => {
-    setSelectedSeat(seatNumber);
+  const handleSelect = (seats) => {
+    setSelectedSeat(seats);
+  };
+
+  const handleProceedClick = () => {
+    setVisible(true);
+    setProceedClicked(true);
   };
 
   const renderButton = () => {
@@ -55,13 +63,18 @@ export default function ResponsiveDialog(props) {
   return (
     <div>
       <div className="responsive-card-container">
-        <Card className="dialog-card">
-          <h1 className="dialog-title">Kindly declare your seating preference</h1>
-          <p className="dialog-button-container">{renderButton()}</p>
-        </Card>
+        {proceedClicked ? null : (
+          <Card className="md:w-25rem shadow-none m-5">
+            <h1 className="text-center">Kindly declare your seating preference</h1>
+            <p className="mx-auto container text-center p-5">{renderButton()}</p>
+            <button className="mx-auto container" onClick={handleProceedClick}>
+              Press
+            </button>
+          </Card>
+        )}
       </div>
 
-      {selectedSeat !== null && (
+      {proceedClicked && selectedSeat !== null && (
         <div className="selected-seat-container">
           <img
             src={seatImageMap[selectedSeat]}
@@ -70,6 +83,19 @@ export default function ResponsiveDialog(props) {
           />
           <p className="selected-seat-label">Selected Seat: {selectedSeat}</p>
         </div>
+      )}
+
+      <Dialog
+        header="Header"
+        visible={visible}
+        onHide={() => setVisible(false)}
+        style={{ width: "50vw" }}
+        breakpoints={{ "960px": "75vw", "641px": "100vw" }}
+      >
+        <p className="mx-auto container text-center p-5">{renderButton()}</p>
+      </Dialog>
+      {proceedClicked && selectedSeat !== null && (
+        <Theater movieId={props.movieId} noOfSeats={parseInt(selectedSeat)} />
       )}
     </div>
   );
